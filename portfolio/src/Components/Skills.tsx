39406ba080ed42bf1a9e0d1c43f6ba3dc4ef1734
furkanguvenme <furkanguven.me@gmail.com>
2025-02-19 14:01:59 +0300

    Skill Effects

--- portfolio/src/Components/Skills.tsx
+++ portfolio/src/Components/Skills.tsx
@@ -77,10 +77,16 @@ export const Skills = () => {
         <Header/>
         <div className="grid grid-flow-row grid-cols-3 md:grid-flow-row md:grid-cols-5 gap-9 px-1 py-9 md:py-6 md:gap-x-14 xl:gap-x-28">
             {Object.entries(skillData.skills).map((item)=>(
-                <div className="flex flex-col gap-4 items-center">
-                    <img className="w-16 h-16 md:w-24 md:h-24 xl:w-32 xl:h-32" src={item[1].iconLink}/>
-                    <p className="text-white text-xl">{item[1].name}</p>
-                </div>
+                <div className="flex flex-col gap-4 items-center group">
+                <img
+                  className="w-16 h-16 md:w-24 md:h-24 xl:w-32 xl:h-32 transition-transform duration-300 group-hover:scale-110 group-hover:brightness-125"
+                  src={item[1].iconLink}
+                />
+                <p className="text-white text-xl transition-all duration-300 group-hover:text-lime-500 group-hover:opacity-100 opacity-80">
+                  {item[1].name}
+                </p>
+              </div>
+              
             ))}
         </div>
         <Footer/>
